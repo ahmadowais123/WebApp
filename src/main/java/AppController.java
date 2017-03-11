da6396b04ddf0b775362129c0a5191d72b608e6b
ahmadowais123 <owais.khan2@mail.mcgill.ca>
2017-03-11 00:02:22 -0700

    Create initial project

---
+++ src/main/java/AppController.java
@@ -0,0 +1,60 @@
+package com.owais;
+
+import com.owais.dao.MemberRepository;
+import com.owais.model.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api")
+public class AppController {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/hello")
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello World!!";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/create")
+    public ResponseEntity createMember(
+            @RequestParam("name") String name,
+            @RequestParam("studentId") String studentId,
+            @RequestParam("phoneNo") String phoneNo,
+            @RequestParam("dateOfBirth") String dateOfBirth
+    ) {
+        Member member = new Member(name, studentId, phoneNo, dateOfBirth);
+        memberRepository.save(member);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    //Return all members
+    @RequestMapping(method = RequestMethod.GET, value = "/members")
+    public ResponseEntity findMembers() {
+        Iterable<Member> listMembers = memberRepository.findAll();
+
+        return new ResponseEntity<>(listMembers, HttpStatus.FOUND);
+
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/member")
+    public ResponseEntity findMember(
+            @RequestParam("studentId") String studentId
+    ) {
+        List<Member> members = memberRepository.findByStudentId(studentId);
+
+        if(members.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(members, HttpStatus.FOUND);
+    }
+}
